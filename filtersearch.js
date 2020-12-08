d020b0bc72ba2eb3f69519406ef2d8b9d1735359
AnishH210 <anish.hubert@mca.christuniversity.in>
2020-12-08 15:12:55 +0530

    Update filtersearch.js

--- filtersearch.js
+++ filtersearch.js
@@ -2,11 +2,10 @@ angular.module("myapp",[])
 .controller("myctrl",function($scope)
 {
     var PC=[
-        {id:'p01',name:'Core i9 10850KF',vendor:'Intel',price:50000,quantity:15,cond:"New"},
-        {id:'p02',name:'DDR4 8GB 3666MHz',vendor:'Corsair',price:4000,quantity:50,cond:"New"},
-        {id:'p03',name:'Z490 Strix',vendor:'Asus',price:35000,quantity:4,cond:"Refurb"},
-        {id:'p04',name:'ML360R',vendor:'CM',price:13000,quantity:5,cond:"Openbox"},
-        {id:'p05',name:'Ryzen 3950X',vendor:'AMD',price:40000,quantity:1,cond:"Used"}
+        {id:'p01',name:'10850K',vend:'Intel',price:10000,quantity:7,cond:"New"},
+        {id:'p02',name:'GTX 1080',vend:'Nvidea',price:17300,quantity:2,cond:"Good"},
+        {id:'p03',name:'RTX 2080',vend:'Nvidea',price:20000,quantity:12,cond:"New"},
+        {id:'p04',name:'RAM DDR4',vend:'Crucial',price:4350,quantity:40,cond:"Used"}
     ];
     console.log(PC);
     $scope.PC=PC;
